e05676eb8d29d8edb8809982f2bbd6819ff37d1d
adad22ro <gabirusu2000@gmail.com>
2024-03-17 21:11:31 +0200

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'pages/SharedLayout';
+// import MoviesPage from 'pages/Movies/MoviesPage';
+
+const HomePage = lazy(() => import('pages/Home/HomePage'));
+const MoviesPage = lazy(() => import('pages/Movies/MoviesPage'));
+const NotFoundPage = lazy(() => import('pages/NotFound/NotFoundPage'));
+const MovieDetailsPage = lazy(() =>
+  import('pages/MovieDetails/MovieDetailsPage')
+);
+const Cast = lazy(() => import('pages/MovieDetails/components/Cast/Cast'));
+const Reviews = lazy(() =>
+  import('pages/MovieDetails/components/Reviews/Reviews')
+);
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" element={<MoviesPage />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
+
+export default App;
